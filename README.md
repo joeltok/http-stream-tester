5430afe7d1c378f3bd1c883cf7f9d982069c667d
Joel Home Server <jttctc@gmail.com>
2020-11-13 18:42:36 +0800

    add README.md

---
+++ README.md
@@ -0,0 +1,89 @@
+# HTTP Stream Tester
+
+A library to test streaming of data to HTTP endpoints for ExpressJS Applications. Promise-based.
+
+## Installation
+
+```
+npm install --save-dev http-stream-tester
+```
+
+## Example usage
+app.js
+```js
+const express = require('express')
+
+app = express();
+app.post('/route',(req, res) => {
+  req.on('data', (chunk) => {
+    console.log(data)
+  });
+  req.on('end', () => {
+    console.log('received.')
+    res.send({})
+  })
+})
+```
+
+app.test.js
+```js
+const httpstreamtester = require('httpstreamtester')
+const app = require('./app')
+
+describe('http-stream-tester', () => {
+  it('simple post request with stream', async () => {
+    const msgs = ['some input a', 'some input b']
+    const source = Readable.from(msgs)
+    const res = await httpstreamtester(app, source, {
+      route: '/route', 
+      method: 'post',
+    })
+
+    expect(res.status).toEqual(200)
+  })
+})
+```
+
+run
+```sh
+npm test
+> http-stream-tester@0.1.0 test /path/to/application
+> jest --env=node
+
+ PASS  ./app.test.js
+  httpstreamtester
+    ✓ simple post request with stream (36 ms)
+
+Test Suites: 1 passed, 1 total
+Tests:       1 passed, 1 total
+Snapshots:   0 total
+Time:        0.1 s, estimated 1 s
+Ran all test suites.
+```
+
+## Note when using with Jest
+
+When using with Jest, ensure that Jest runs in the node env by setting the following in `package.json`
+```
+...
+  "scripts": {
+    "test": "jest --env=node"
+  },
+...
+```
+If not, we will get a `Error: Cross origin http://localhost forbidden` error.
+
+## Arguments
+
+- app: An express server
+- source: a readable stream, or a path to a file
+- options: 
+  - route: the route on the express app
+  - method: get, post, put, etc. default: post
+  - contentType: the content-type header; default: application/octet-stream
+  - axiosOptions: all options that the [axios](https://www.npmjs.com/package/axios) package supports.
+
+
+  ## Why axios?
+
+  This library spins up the app as a server internally, and has it listen to open ports on localhost. It then uses [axios](https://www.npmjs.com/package/axios) to write streams to the application via these ports. It shuts down the app/server automatically after tests are complete.
--- index.js
+++ index.js
@@ -2,6 +2,70 @@ const path = require('path')
 const axios = require('axios')
 const fs = require('fs')
 
+module.exports =  async (app, source, options) => {
+  validateApp(app)
+  validateSource(source)
+
+  const {
+    route = '/',
+    method = 'post',
+    contentType = 'application/octet-stream',
+    axiosOptions = {},
+  } = options
+
+  const listener = await listenOnApp(app)
+  const { port } = listener.address()
+
+  try {
+    const stream = handleStream(source)
+    const headers = generateHeaders(axiosOptions, contentType)
+      
+    options = Object.assign({}, {
+      method: method,
+      url: new URL(route, `http://127.0.0.1:${port}`).href,
+      headers: headers,
+      data: stream,
+    }, axiosOptions)
+    const res = await axios(options)
+
+    listener.close()
+    return res;  
+  } catch (e) {
+    listener.close()
+    throw e
+  }
+
+}
+
+async function listenOnApp(app) {
+  let listener;
+  const port = await new Promise(async (resolve, reject) => {
+    listener = await app.listen(0, () => {
+      resolve(listener)
+    })
+  })
+  return listener
+}
+
+function handleStream(source) {
+  let stream;
+  if (typeof source === 'string') {
+    stream = fs.createReadStream(source)
+  } else {
+    stream = source
+  }
+  return stream
+}
+
+function generateHeaders(axiosOptions, contentType) {
+  // handle headers specially
+  const headers = Object.assign({
+    'content-type': contentType,
+  }, axiosOptions.headers)
+  delete axiosOptions['headers']
+  return headers
+}
+
 function validateApp(app) {
   try {
     app.listen
@@ -28,55 +92,3 @@ function validateSource(source) {
 
   throw new Error('source must be a stream or a path to a file')
 }
-
-const httpstreamtest = async (app, source, options) => {
-  validateApp(app)
-  validateSource(source)
-
-  const {
-    route = '/',
-    method = 'post',
-    contentType = 'application/octet-stream',
-    axiosOptions = {},
-  } = options
-
-  let listener;
-  const port = await new Promise(async (resolve, reject) => {
-    listener = await app.listen(0, () => {
-      const { port } = listener.address()
-      resolve(port)
-    })
-  })
-
-  try {
-    let stream;
-    if (typeof source === 'string') {
-      stream = fs.createReadStream(source)
-    } else {
-      stream = source
-    }
-  
-    // handle headers specially
-    headers = Object.assign({
-      'content-type': contentType,
-    }, axiosOptions.headers)
-    delete axiosOptions['headers']
-  
-    options = Object.assign({}, {
-      method: method,
-      url: new URL(route, `http://127.0.0.1:${port}`).href,
-      headers: headers,
-      data: stream,
-    }, axiosOptions)
-  
-    const res = await axios(options)
-    listener.close()
-    return res;  
-  } catch (e) {
-    listener.close()
-    throw e
-  }
-
-}
-
-module.exports = httpstreamtest;
