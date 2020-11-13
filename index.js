7f23f2ceea56685ca8e9c7e35739063f46319c5e
Joel Home Server <jttctc@gmail.com>
2020-11-13 18:07:46 +0800

    add more tests

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
 const path = require('path')
 const axios = require('axios')
+const fs = require('fs')
 
-function validateSource(source) {
+function validateApp(app) {
+  try {
+    app.listen
+  } catch (e) {
+    throw new Error('app should be an express server')
+  }
 
+  if (!app.listen) {
+    throw new Error('app should be an express server')
+  }
 }
 
-const httpstreamtest = async (app, route, source, axiosOptions={}) => {
+function validateSource(source) {
+  if (typeof source === 'string') {
+    if (!fs.existsSync(source)) {
+      throw new Error('file does not exist: ' + source)
+    } 
+    return true
+  }
+
+  if (source.pipe) {
+    return true
+  }
+
+  throw new Error('source must be a stream or a path to a file')
+}
+
+const httpstreamtest = async (app, source, options) => {
+  validateApp(app)
+  validateSource(source)
+
+  const {
+    route = '/',
+    method = 'post',
+    contentType = 'application/octet-stream',
+    axiosOptions = {},
+  } = options
+
   let listener;
   const port = await new Promise(async (resolve, reject) => {
     listener = await app.listen(0, () => {
@@ -14,25 +48,35 @@ const httpstreamtest = async (app, route, source, axiosOptions={}) => {
     })
   })
 
-  let stream;
-  if (typeof source === 'string') {
-    stream = fs.createReadStream(source)
-  } else {
-    stream = source
+  try {
+    let stream;
+    if (typeof source === 'string') {
+      stream = fs.createReadStream(source)
+    } else {
+      stream = source
+    }
+  
+    // handle headers specially
+    headers = Object.assign({
+      'content-type': contentType,
+    }, axiosOptions.headers)
+    delete axiosOptions['headers']
+  
+    options = Object.assign({}, {
+      method: method,
+      url: new URL(route, `http://127.0.0.1:${port}`).href,
+      headers: headers,
+      data: stream,
+    }, axiosOptions)
+  
+    const res = await axios(options)
+    listener.close()
+    return res;  
+  } catch (e) {
+    listener.close()
+    throw e
   }
 
-  options = Object.assign({
-    method: 'post',
-    url: new URL(route, `http://127.0.0.1:${port}`).href,
-    headers: {
-      'content-type': 'application/octet-stream',
-    },
-    data: stream,
-  }, axiosOptions)
-
-  const res = await axios(options)
-  listener.close()
-  return res;
 }
 
 module.exports = httpstreamtest;
--- index.test.js
+++ index.test.js
@@ -1,28 +1,106 @@
+const fs = require('fs')
 const { Readable } = require('stream')
 const express = require('express')
 const httpstreamtest = require('./index')
 
 describe('httpstreamtest', () => {
-  it('simple post request with stream', async () => {
-    const server = express();
+  let server;
+
+  beforeAll(() => {
+    server = express();
     server.post('/route',(req, res) => {
       let chunksReceived = []
       req.on('data', (chunk) => {
         chunksReceived.push(chunk.toString())
       });
       req.on('end', () => {
-        res.send(chunksReceived)
+        res.send({
+          contentType: req.headers['content-type'],
+          message: chunksReceived
+        })
       })
     })
-
-    const msgs = ['some input a', 'some input b']
-    const readStream = Readable.from(msgs)
-    const res = await httpstreamtest(server, '/route', readStream)
-
-    expect(res.status).toEqual(200)
-    expect(res.data).toEqual(msgs)
   })
 
-  it.skip('simple post request with file', async () => {})
-  it.skip('post request with overriding axios options', async () => {})
+  it('simple post request with stream', async () => {
+    const msgs = ['some input a', 'some input b']
+    const readStream = Readable.from(msgs)
+    const res = await httpstreamtest(server, readStream, {
+      route: '/route', 
+      method: 'post',
+    })
+
+    expect(res.status).toEqual(200)
+    expect(res.data).toEqual({
+      contentType: 'application/octet-stream',
+      message: msgs
+    })
+  })
+
+  it('simple post request with stream with empty headers', async () => {
+    const msgs = ['some input a', 'some input b']
+    const readStream = Readable.from(msgs)
+    const res = await httpstreamtest(server, readStream, {
+      route: '/route',
+      method: 'post',
+      axiosOptions: {
+        headers: {}
+      }
+    })
+
+    expect(res.status).toEqual(200)
+    expect(res.data).toEqual({
+      contentType: 'application/octet-stream',
+      message: msgs
+    })
+  })
+
+  it('simple post request with file', async () => {
+    const readStream = fs.createReadStream('./fixtures/example.txt')
+    const res = await httpstreamtest(server, readStream, {
+      route: '/route',
+      method: 'post',
+      axiosOptions: {
+        headers: {}
+      }
+    })
+
+    expect(res.status).toEqual(200)
+    expect(res.data).toEqual({
+      contentType: 'application/octet-stream',
+      message: ['I am a line\nI am another line\n'],
+    })
+  })
+
+  it('validate is express app', async () => {
+    let errorCaughtMessage = '';
+    try {
+        const msgs = ['some input a', 'some input b']
+        const readStream = Readable.from(msgs)
+        const res = await httpstreamtest(null, readStream, {
+        route: '/route', 
+        method: 'post',
+      })
+    } catch (err) {
+      errorCaughtMessage = err.message
+    }
+
+    expect(errorCaughtMessage).toEqual('app should be an express server')
+  })
+
+  it('validate source exists', async () => {
+    let errorCaughtMessage = '';
+    try {
+      const res = await httpstreamtest(server, './non_existent_file.txt', {
+        route: '/route', 
+        method: 'post',
+      })
+    } catch (err) {
+      errorCaughtMessage = err.message
+    }
+
+    expect(errorCaughtMessage).toEqual(
+      'file does not exist: ./non_existent_file.txt'
+    )
+  })
 })
